fb126ffde291830af01a9c58e4fb87c9106c4177
Ashutosh-777 <112700624+Ashutosh-777@users.noreply.github.com>
2023-06-07 01:07:17 +0530

    Update movies.js

--- movies.js
+++ movies.js
@@ -1,21 +1,21 @@
 const moviesContainer = document.getElementById("moviesContainer");
 
-// Function to fetch movie data from the API
+
 async function fetchMovies() {
   try {
     const response = await fetch("https://api.themoviedb.org/3/trending/all/day?api_key=0c1881819b36de377c0212f81949a9a3"); // Replace with your API endpoint
     const data = await response.json();
-    const movies = data.results; // Assuming the API response contains an array of movies
+    const movies = data.results;
     
-    // Iterate over the movies and create a card for each movie
+    
     movies.forEach((movie) => {
       const card = document.createElement("div");
       card.classList.add("card");
       
       const poster = document.createElement("img");
-      poster.src ='http://image.tmdb.org/t/p/w500'+ movie.backdrop_path;  // Replace with the appropriate property from your API response
+      poster.src ='http://image.tmdb.org/t/p/w500'+ movie.backdrop_path; 
       poster.alt = movie.title||movie.name;
-      poster.vote=movie.vote_average; // Replace with the appropriate property from your API response
+      poster.vote=movie.vote_average;
       poster.classList.add("poster");
       
       const overlay = document.createElement("div");
@@ -26,7 +26,7 @@ async function fetchMovies() {
       title.textContent = movie.title||movie.name; 
       const vote = document.createElement("h1");
       vote.classList.add("title");
-      vote.textContent = movie.vote_average;// Replace with the appropriate property from your API response
+      vote.textContent = movie.vote_average;
       overlay.appendChild(title);
       overlay.appendChild(vote);
       card.appendChild(poster);
@@ -37,6 +37,4 @@ async function fetchMovies() {
     console.error("Error fetching movies:", error);
   }
 }
-
-// Call the fetchMovies function to retrieve and display the movies
 fetchMovies();
